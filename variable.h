c9b95848b4e4ff93afd2beb6fe9593709639852c
naidy <allen830523@gmail.com>
2017-10-16 18:21:36 +0800

    hw3

--- variable.h
+++ variable.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include "atom.h"
+#include "struct.h"
 using std::string;
 
 class Variable : public Term{
@@ -11,7 +12,11 @@ public:
   Variable(string s):_symbol(s), _value(s){}
   string const _symbol;
   string symbol() const {return _symbol;}
-  string value() const { return _value; }
+  string value() const {
+    if (_s)
+      return _s->value();
+    return _value; 
+  }
   bool isMatch(){return !_assignable;}
   bool match(Variable & v){
     for (int i = 0; i < _match.size(); i++){
@@ -27,6 +32,14 @@ public:
     for (int i = 0; i < _match.size(); i++)
       _match[i]->match(v);
     return true;
+  }
+  bool match(Struct & s){
+    bool ret = _assignable;
+    if(_assignable){
+      _s = &s ;
+      _assignable = false;
+    }
+    return ret;
   }
   bool match(Term & term){
     if (_value == term.value())
@@ -45,6 +58,7 @@ private:
   string _value;
   bool _assignable = true;
   std::vector<Variable*> _match;
+  Struct* _s;
 };
 
 bool Atom::match(Variable & v){
