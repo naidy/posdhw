b7a7e56a0cd7c74dedb36122e331b1919e538686
naidy <allen830523@gmail.com>
2017-10-16 17:28:42 +0800

    hw3

--- number.h
+++ number.h
@@ -6,7 +6,7 @@
 
 using std::string;
 
-namespace util { template <typename T> std::string to_string(const T& t) { std::string str{std::to_string (t)}; int offset{1}; if (str.find_last_not_of('0') == str.find('.')) { offset = 0; } str.erase(str.find_last_not_of('0') + offset, std::string::npos); return str; } }
+//namespace util { template <typename T> std::string to_string(const T& t) { std::string str{std::to_string (t)}; int offset{1}; if (str.find_last_not_of('0') == str.find('.')) { offset = 0; } str.erase(str.find_last_not_of('0') + offset, std::string::npos); return str; } }
 
 class Variable;
 
